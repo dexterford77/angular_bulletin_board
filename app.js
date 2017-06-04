3a14b16ce36d9a96a6d5d34020efce411c2a14dd
Dexter Ford <dexter.ford.77@gmail.com>
2017-06-03 23:27:50 -0500

    add mucho

--- app.js
+++ app.js
@@ -1,1 +1,67 @@
-var bulletinBoard = angular.module("bulletinBoard", []);
+var BB = angular.module("BB", []);
+
+BB.controller("PostsCtrl", ["$scope", "PostService", "CommentService",
+  function($scope, PostService, CommentService){
+    $scope.init = function(){
+      $scope.getComments();
+      $scope.getPosts();
+    }
+    $scope.getPosts = function(){
+      PostService.all().then(function(posts){
+        $scope.posts = posts;
+      });
+    }
+    $scope.getComments = function(){
+      CommentService.all().then(function(comments){
+        $scope.comments = comments;
+      })
+    }
+}]);
+
+BB.service("_", ["$window", function($window){
+  return $window._
+}]);
+
+BB.service("PostService", ['$http', '_', 'CommentService', 
+  function($http, _, CommentService){
+    var _posts;
+    var _extendPost = function(post){
+      _comments = [];
+      post.commentIds.forEach(function(id){
+        _comment = CommentService.getCommentById(id);
+        _comments.push(_comment);
+      })
+      post.comments = _comments;
+    }
+    var _extendPosts = function(posts){
+      _.each(posts, function(post){
+        _extendPost(post);
+      })
+    }
+    this.all = function(){
+      var endpoint = "data/posts.json";
+      _extendPosts();
+      return $http.get(endpoint)
+      .then(function(response){
+        _posts = response.data;
+        _extendPosts(_posts);
+        return _posts
+      })
+    }
+}]);
+
+BB.service("CommentService", ['$http',
+  function($http){
+    var _comments;
+    this.all = function(){
+      var endpoint = "data/comments.json";
+      return $http.get(endpoint)
+      .then(function(response){
+        _comments = response.data;
+        return _comments
+      })
+    }
+    this.getCommentById = function(id){
+      return _comments[id]
+    }
+}]);
